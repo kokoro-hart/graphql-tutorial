515122feaa0b5d40d3621c8db6732cc530ec5e9a
kokoro-hart <tobita@wd-flat.com>
2024-05-04 13:23:30 +0900

    feat: タスク削除機能の実装

--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -36,4 +36,10 @@ export class TaskService {
       where: { id },
     });
   }
+
+  async deleteTask(id: number): Promise<Task> {
+    return await this.prismaService.task.delete({
+      where: { id },
+    });
+  }
 }
